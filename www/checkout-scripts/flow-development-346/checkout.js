f0fe7ee160df557e2b83ffc5d2ac582f66212f1c
Michaeal Palma <michael@Michaeals-MBP.localdomain>
2017-11-27 11:56:33 -0500

    Fixing typo

---
+++ www/checkout-scripts/flow-development-346/checkout.js
@@ -0,0 +1,18 @@
+flow.checkout.onPageView('all', function handlePageView(order) {
+    console.log('*****EVERY', order);
+});
+flow.checkout.onPageView('cart', function handlePageView(order) {
+    console.log('*****CART', order);
+});
+flow.checkout.onPageView('confirmation', function handlePageView(order) {
+    console.log('*****CONFIRMATION', order);
+});
+flow.checkout.onPageView('contactInfo', function handlePageView(order) {
+    console.log('*****CONTACT', order);
+});
+flow.checkout.onPageView('shippingInfo', function handlePageView(order) {
+    console.log('*****SHIPPING', order);
+});
+flow.checkout.onPageView('paymentInfo', function handlePageView(order) {
+    console.log('*****PAYMENT', order);
+});
