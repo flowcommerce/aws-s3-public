ae553ca56b775b74e5c3e666a32739500ddbbaf8
Michaeal Palma <michael@Michaeals-MBP.localdomain>
2017-11-27 13:53:10 -0500

    Updating scripts to use GA

--- www/checkout-scripts/flow-development-346/checkout.js
+++ www/checkout-scripts/flow-development-346/checkout.js
@@ -1,18 +1,41 @@
 flow.checkout.onPageView('all', function handlePageView(order) {
     console.log('*****EVERY', order);
+    ga('send', {
+        hitType: 'pageview'
+    });
 });
 flow.checkout.onPageView('cart', function handlePageView(order) {
     console.log('*****CART', order);
+    ga('send', {
+        hitType: 'pageview',
+        orderId: order.id
+    });
 });
 flow.checkout.onPageView('confirmation', function handlePageView(order) {
     console.log('*****CONFIRMATION', order);
+    ga('send', {
+        hitType: 'pageview',
+        orderId: order.id
+    });
 });
 flow.checkout.onPageView('contactInfo', function handlePageView(order) {
     console.log('*****CONTACT', order);
+    ga('send', {
+        hitType: 'pageview',
+        orderId: order.id
+    });
 });
 flow.checkout.onPageView('shippingMethod', function handlePageView(order) {
     console.log('*****SHIPPING', order);
+    ga('send', {
+        hitType: 'pageview',
+        orderId: order.id
+    });
 });
 flow.checkout.onPageView('paymentInfo', function handlePageView(order) {
     console.log('*****PAYMENT', order);
+    ga('send', {
+        hitType: 'pageview',
+        orderId: order.id
+    });
 });
