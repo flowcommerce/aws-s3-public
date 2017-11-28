74baf608b463b23b3ef6a251727f2a161e7b6053
Michaeal Palma <michael@Michaeals-MBP.localdomain>
2017-11-28 11:28:25 -0500

    Updating scripts

--- www/checkout-scripts/flow-development-346/checkout.js
+++ www/checkout-scripts/flow-development-346/checkout.js
@@ -1,35 +1,35 @@
-flow.checkout.onPageView('all', function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.ALL, function handlePageView(order) {
     console.log('*****EVERY', order);
 });
-flow.checkout.onPageView('cart', function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.CART, function handlePageView(order) {
     console.log('*****CART', order);
     ga('send', {
         hitType: 'pageview',
         orderId: order.id
     });
 });
-flow.checkout.onPageView('confirmation', function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.CONFIRMATION, function handlePageView(order) {
     console.log('*****CONFIRMATION', order);
     ga('send', {
         hitType: 'pageview',
         orderId: order.id
     });
 });
-flow.checkout.onPageView('contactInfo', function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.CONTACT_INFO, function handlePageView(order) {
     console.log('*****CONTACT', order);
     ga('send', {
         hitType: 'pageview',
         orderId: order.id
     });
 });
-flow.checkout.onPageView('shippingMethod', function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.SHIPPING_METHOD, function handlePageView(order) {
     console.log('*****SHIPPING', order);
     ga('send', {
         hitType: 'pageview',
         orderId: order.id
     });
 });
-flow.checkout.onPageView('paymentInfo', function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.PAYMENT_INFO, function handlePageView(order) {
     console.log('*****PAYMENT', order);
     ga('send', {
         hitType: 'pageview',
