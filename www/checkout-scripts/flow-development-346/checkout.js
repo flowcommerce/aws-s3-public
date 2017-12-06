34702485d8830b56e4fb426c07d39c85d96e13eb
Michael Palma <mpalma@flow.io>
2017-12-06 12:05:50 -0500

    Updated checkout scripts

--- www/checkout-scripts/flow-development-346/checkout.js
+++ www/checkout-scripts/flow-development-346/checkout.js
@@ -1,35 +1,35 @@
-flow.checkout.onPageView(flow.checkout.enums.ALL, function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.pageView.ALL, function handlePageView(order) {
     console.log('*****EVERY', order);
 });
-flow.checkout.onPageView(flow.checkout.enums.CART, function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.pageView.CART, function handlePageView(order) {
     console.log('*****CART', order);
     ga('send', {
         hitType: 'pageview',
         orderId: order.id
     });
 });
-flow.checkout.onPageView(flow.checkout.enums.CONFIRMATION, function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.pageView.CONFIRMATION, function handlePageView(order) {
     console.log('*****CONFIRMATION', order);
     ga('send', {
         hitType: 'pageview',
         orderId: order.id
     });
 });
-flow.checkout.onPageView(flow.checkout.enums.CONTACT_INFO, function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.pageView.CONTACT_INFO, function handlePageView(order) {
     console.log('*****CONTACT', order);
     ga('send', {
         hitType: 'pageview',
         orderId: order.id
     });
 });
-flow.checkout.onPageView(flow.checkout.enums.SHIPPING_METHOD, function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.pageView.SHIPPING_METHOD, function handlePageView(order) {
     console.log('*****SHIPPING', order);
     ga('send', {
         hitType: 'pageview',
         orderId: order.id
     });
 });
-flow.checkout.onPageView(flow.checkout.enums.PAYMENT_INFO, function handlePageView(order) {
+flow.checkout.onPageView(flow.checkout.enums.pageView.PAYMENT_INFO, function handlePageView(order) {
     console.log('*****PAYMENT', order);
     ga('send', {
         hitType: 'pageview',
